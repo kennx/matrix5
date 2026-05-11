94f3f49e643084a9b960b03f7ea498018f346939
Kenn <zhangxjian@me.com>
2026-05-11 16:13:37 +0800

    refactor: remove BLE stack and pairing system, add ArduinoJson dependency

--- src/ble_display_text.cpp
+++
@@ -1,8 +0,0 @@
-#include "ble_display_text.h"
-
-std::string formatPairingCodeLine(const std::string& code) {
-    if (code.empty()) {
-        return "CODE ------";
-    }
-    return "CODE " + code;
-}
--- src/ble_display_text.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include <string>
-
-std::string formatPairingCodeLine(const std::string& code);
